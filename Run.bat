771bd7ebc6cd957e6c1b6ecc69adf95f2bad3437
priyaturkhade <priyaturkhade17@gmail.com>
2024-02-09 14:26:01 +0530

    modified

--- Run.bat
+++ Run.bat
@@ -1,4 +1,2 @@
 F:\Pycharm projects\Credkart_pytest1
-pytest -v -s -n=3 --html=HtmlReports/chrome_report.html --browser chrome "F:\Pycharm projects\Credkart_pytest1"
-pytest -v -s -n=3 --html=HtmlReports/firefox_report.html --browser firefox "F:\Pycharm projects\Credkart_pytest1"
-pytest -v -s -n=3 --html=HtmlReports/edge_report.html --browser edge "F:\Pycharm projects\Credkart_pytest1"
+C:\Users\HP\AppData\Local\Programs\Python\Python312\Scripts\pytest.exe -v -s -n=3 --html=HtmlReports/chrome_report.html 
